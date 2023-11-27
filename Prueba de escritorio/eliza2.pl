cad10622a9242a91b473046487da2f7bbec80220
DancyMule <jorgito310801@gmail.com>
2023-11-27 12:32:02 -0600

    Add Spark (own chatbot)

---
+++ Prueba de escritorio/eliza2.pl
@@ -0,0 +1,64 @@
+eliza:-	writeln('Hola , mi nombre es  Eliza tu  chatbot,
+	por favor ingresa tu consulta,
+	usar solo minúsculas sin . al final:'),
+	readln(Input),
+	eliza(Input),!.
+eliza(Input):- Input == ['Adios'],
+	writeln('Adios. espero poder verte ayudado.'), !.
+eliza(Input):- Input == ['Adios', '.'],
+	writeln('Adios. espero poder verte ayudado.'), !.
+eliza(Input) :-
+	template(Stim, Resp, IndStim),
+	match(Stim, Input),
+	% si he llegado aquí es que he
+	% hallado el template correcto:
+	replace0(IndStim, Input, 0, Resp, R),
+	writeln(R),
+	readln(Input1),
+	eliza(Input1), !.
+
+template([eliza, quien, es, el, hijo, de, s(_)], [flagfam],[6]). %Esto va a linea 43 si el contenido esta completo y correto.
+																 % Si esta incompleto, vacio o la informacion es incorrecta el input va a la linea 43, luego a la 50 y luego a la 41  
+template(_, ['Please', explain, a, little, more, '.'], []). 
+
+padre(juan, luis).		 
+padre(juan, antonio).
+padre(luis, carlos). 
+familia(X,R) :- padre(X,Y), R=["El  hijo de ", X, "es", Y].		 
+		 
+match([],[]).
+match([], _):- true.
+
+match([S|Stim],[I|Input]) :-
+	atom(S), % si I es un s(X) devuelve falso
+	S == I,
+	match(Stim, Input),!.
+
+match([S|Stim],[_|Input]) :-
+% I es un s(X), lo ignoro y continúo con el resto de la lista
+	\+atom(S),
+	match(Stim, Input),!.
+
+replace0([], _, _, Resp, R):- append(Resp, [], R),writeln('Entre a linea 41'),!.
+
+replace0([I|_], Input, _, Resp, R):-
+	nth0(I, Input, Atom),
+	nth0(0, Resp, X),
+	X == flagfam,
+	writeln('Entre a linea 43'),
+	familia(Atom, R).
+		 
+		 
+replace0([I|Index], Input, N, Resp, R):-
+	length(Index, M), M =:= 0,
+	nth0(I, Input, Atom),
+	writeln('Entre a linea 50'),
+	select(N, Resp, Atom, R1), append(R1, [], R),!.
+
+replace0([I|Index], Input, N, Resp, R):-
+	nth0(I, Input, Atom),
+	length(Index, M), M > 0,
+	select(N, Resp, Atom, R1),
+	writeln('Entre a linea 56'),
+	N1 is N + 1,
+	replace0(Index, Input, N1, R1, R),!.
